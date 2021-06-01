4d8c534ca141317205d1835697c902eadeda3f1a
K <kristian.gjerde@gmail.com>
2021-06-01 12:31:20 +0200

    Fix bug in English tokenization

--- inst/python/tokenization_en.py
+++ inst/python/tokenization_en.py
@@ -5,5 +5,5 @@ def tokenize_sentences_en(list_of_strings):
     setninger = [sent_tokenize(text, language = "english") for text in tqdm(list_of_strings)]
     # Tillegg: Newline regnes som setningsavslutning:
     setninger = [[[x for x in line.split("\n") if x] for line in doc] for doc in setninger]
-    setninger = [flatten_list(doc) for doc in setninger]
+    setninger = [(i, flatten_list(doc)) for i,doc in enumerate(setninger)]
     return setninger
