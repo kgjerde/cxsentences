3cc05d8ca9a46d0ed74e32045b2d095b9f66825e
kgjerde <kristian.gjerde@gmail.com>
2025-04-02 13:09:56 +0200

    droplet path

--- R/cx_setup.R
+++ R/cx_setup.R
@@ -3,7 +3,7 @@
 #' @param virtenv Path to virtenv with required modules
 #' @export
 cx_setup <-
-  function(virtenv = "/Users/kristianlg/Library/CloudStorage/Dropbox/Projects/corporaexplorer_development/cxsentences/inst/python/.venv/") {
+  function(virtenv = "/Users/Kristian/.local/share/virtualenvs/apekatt-mr9vg8U1/") {
     reticulate::use_virtualenv(virtenv, required = TRUE)
 
     path_to_py_functions <- system.file("python", package = "cxsentences")
